59455469223bada35c6a4c7ebcf3d68c2044b1b5
muhammed050 <73080022+muhammed050@users.noreply.github.com>
2026-08-17 00:51:37 +0300

    chore: remove generated Next.js output from v2 branch

--- .next/static/chunks/app/not-found-fa221825e9f84436.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[314,345,394,698],{8476:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2619,23))}},e=>{e.O(0,[619,441,255,358],()=>e(e.s=8476)),_N_E=e.O()}]);
